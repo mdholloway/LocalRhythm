887a00cf57437563d2844951914ada89cd25bff7
Michael Holloway <michaeldavidholloway@gmail.com>
2015-01-05 15:10:45 -0500

    refactored location handling code

--- app/src/main/java/co/hollowlog/localrhythm/LandingActivity.java
+++ app/src/main/java/co/hollowlog/localrhythm/LandingActivity.java
@@ -2,11 +2,12 @@ package co.hollowlog.localrhythm;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -26,19 +27,20 @@ import java.util.Locale;
 public class LandingActivity extends Activity {
 
     public static Activity la;
-    private LocationAgent mLocationAgent;
+    protected LocationManager mLocationManager;
     private List<Address> mAddresses;
     public double mCurrentLat;
     public double mCurrentLong;
     public String mStreetAddress;
     public String mCityStateZip;
     public String mCityName;
+    public static final String ACTION_LOCATION = "co.hollowlog.localrhythm.ACTION_LOCATION";
 
-    private LocationReceiver mLocationReceiver = new LocationReceiver(this) {
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
 
         @Override
         public void onReceive(Context context, Intent intent){
-            super.onReceive(context, intent);
+
             Location loc = intent.getParcelableExtra(LocationManager.KEY_LOCATION_CHANGED);
             if (loc != null){
                 mCurrentLat = loc.getLatitude();
@@ -70,6 +72,12 @@ public class LandingActivity extends Activity {
         }
     };
 
+    private PendingIntent getLocationPendingIntent(boolean shouldCreate){
+        Intent broadcast = new Intent(ACTION_LOCATION);
+        int flags = shouldCreate ? 0 : PendingIntent.FLAG_NO_CREATE;
+        return PendingIntent.getBroadcast(this, 0, broadcast, flags);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,25 +88,20 @@ public class LandingActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
-
+        mLocationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        this.registerReceiver(mBroadcastReceiver, new IntentFilter(ACTION_LOCATION));
         boolean gps_enabled = false, network_enabled = false;
 
-        mLocationAgent = LocationAgent.get(this);
+        //check to see if location providers are enabled
+        gps_enabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        network_enabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-        try{
-            gps_enabled = LocationAgent.sLocationAgent.mLocationManager
-                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
-        }catch(Exception ex){}
-
-        try{
-            network_enabled = LocationAgent.sLocationAgent.mLocationManager
-                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        }catch(Exception ex){}
-
+        // prompt user to enable location provider(s) if disabled
         if(!gps_enabled && !network_enabled){
             AlertDialog.Builder dialog = new AlertDialog.Builder(this);
             dialog.setMessage("LocalRhythm requires access to your location. " +
                     "Open settings now to enable?");
+            // if yes, go to settings
             dialog.setPositiveButton("Open",
                     new DialogInterface.OnClickListener() {
 
@@ -108,6 +111,7 @@ public class LandingActivity extends Activity {
                             startActivity(myIntent);
                         }
                     });
+            // quit if user declines to enable a location provider
             dialog.setNegativeButton("Cancel",
                     new DialogInterface.OnClickListener() {
 
@@ -117,33 +121,24 @@ public class LandingActivity extends Activity {
                         }
                     });
             dialog.show();
-
         }
 
-        mLocationAgent.startLocationUpdates();
+        PendingIntent pi = getLocationPendingIntent(true);
+        mLocationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, pi);
+        mLocationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, pi);
 
         final ProgressWheel wheel = (ProgressWheel) findViewById(R.id.progress_wheel);
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 wheel.spin();
             }
         }, 2000);
-
-        this.registerReceiver(mLocationReceiver, new IntentFilter(LocationAgent.ACTION_LOCATION));
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        mLocationAgent.stopLocationUpdates();
-        this.unregisterReceiver(mLocationReceiver);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mLocationAgent.startLocationUpdates();
+        this.unregisterReceiver(mBroadcastReceiver);
     }
 }
